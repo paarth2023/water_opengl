40da72c077b38ae976998dbe0e3f0f86bb3416a2
paarth <paarth@mx>
2026-08-05 18:44:14 +0530

    cleaned up some of the comments that were written in main.cpp that held no purpose.

--- src/main.cpp
+++ src/main.cpp
@@ -22,12 +22,6 @@ struct Particle {
     glm::vec3 color;
 };
 
-// struct Particle {
-//     glm::vec3 center;
-//     float radius;
-//     glm::vec3 color;
-//     // to be added -> density, velocity, etc.
-// };
 
 int main()
 {
@@ -81,39 +75,20 @@ int main()
     layout1.Push<float>(3);
     VertexArray va1;
     va1.AddBuffer(vb1, layout1);
-    // std::vector<glm::vec3> points = {
-    //     glm::vec3(-0.5, 0.0f, 0.0f),
-    //     glm::vec3(0.0f, 0.5f, 0.0f),
-    //     glm::vec3(0.5f, 0.0f, 0.0f)};
-    // VertexBuffer vb(points.data(), points.size() * sizeof(glm::vec3));
-
-    // BufferLayout layout;
-    // layout.Push<float>(3);
-
-    // VertexArray va;
-    // va.AddBuffer(vb, layout);
 
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // 1. Global State Configuration
         glEnable(GL_PROGRAM_POINT_SIZE);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        // 2. DRAW PARTICLES
         newShader.use();
-        // If your shader uses matrices, set them here:
-        // newShader.setMat4("u_projection", projectionMatrix);
         va1.Bind();
         glDrawArrays(GL_POINTS, 0, static_cast<GLsizei>(particles.size()));
 
-        // 3. DRAW TRIANGLES
-        // ourShader.use();
-        // va.Bind();
-        // glDrawArrays(GL_TRIANGLES, 0, static_cast<GLsizei>(points.size()));
 
         glfwSwapBuffers(window);
         glfwPollEvents();
